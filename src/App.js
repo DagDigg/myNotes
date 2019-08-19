131f7cf63bda917befa121108fb8c5355d531957
Claudio D'Agostino <dagdigg@Claudios-MacBook-Pro.local>
2019-08-19 19:01:51 +0200

    :sparkles: Improved app and added nav sections

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "./Routes";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">myNotes</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
+        <Navbar fluid collapseOnSelect className="nav">
+          <Navbar.Brand>
+            <Link to="/" className="logo">
+              myNotes
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/signup">
+                <Nav.Link>Sign Up</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
+        <Routes />
       </div>
     );
   }
---
+++ src/containers/NotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () => (
+  <div className="NotFound">
+    <h3>Oops: couldn't find the page.</h3>
+  </div>
+);
