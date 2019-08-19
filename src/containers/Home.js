cebb0295ee8b535b62b5768fa94d9233ea2a3489
Claudio D'Agostino <dagdigg@Claudios-MacBook-Pro.local>
2019-08-19 15:22:39 +0200

    :sparkles: Added HomePage

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>myNotes</h1>
+          <p>Simple notes taking app.</p>
+        </div>
+      </div>
+    );
+  }
+}
