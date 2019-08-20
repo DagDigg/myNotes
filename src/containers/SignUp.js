1bc12817004442b9268dee50049cf2488b80358d
Claudio D'Agostino <dagdigg@Claudios-MacBook-Pro.local>
2019-08-20 19:01:27 +0200

    :sparkles: Added Sign up boilerplate

---
+++ src/containers/SignUp.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import "./Signup.css";
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      email: "",
+      password: "",
+      confirmPassword: "",
+      confirmationCode: "",
+      newUser: null
+    };
+  }
+
+  validateForm() {
+    return (
+      this.state.email.length > 0 &&
+      this.state.password.length >= 8 &&
+      this.state.password === this.state.confirmPassword
+    );
+  }
+}
