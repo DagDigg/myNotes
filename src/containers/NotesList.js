f15ec6571ac4bdb56e99434d9d90a44f51a5ce81
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-09 20:02:18 +0200

    :sparkles: Styled and added multiple tables to NotesList

--- src/containers/NotesList.js
+++ src/containers/NotesList.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { LinkContainer } from "react-router-bootstrap";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { API } from "aws-amplify";
 import styled from "styled-components";
 
-const NoteContainer = styled(LinkContainer)`
+const NoteContainerLink = styled(LinkContainer)`
   user-select: none;
   padding: 16px;
   margin: 10px;
   transition: transform 0.2s linear;
+`;
+
+const NoteCard = styled.div`
+  background-color: #efefe1;
+  width: 250px;
+  border-radius: 5px;
+  border: 1px solid #444b6e;
+`;
+
+const TablesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
 `;
 
 const reorder = (list, startIndex, endIndex) => {
@@ -20,9 +32,24 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
+//Moves from one list to another
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destinationClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destinationClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destinationClone;
+
+  return result;
+};
+
 const getItemStyle = (isDragging, draggableStyle) => ({
   // change background colour and scale if dragging
-  background: isDragging ? "lightblue" : "white",
+  background: isDragging ? "lightblue" : "#efefe1",
   transform: `scale(${isDragging ? 1.025 : 1})`,
 
   // styles we need to apply on draggables
@@ -34,15 +61,31 @@ class NotesList extends Component {
     super(props);
 
     this.state = {
-      notes: this.props.notes
+      notes: this.props.notes,
+      tables: [],
+      test: [
+        { content: "test", noteId: "abcdef" },
+        { content: "test2", noteId: "abcdef" }
+      ]
     };
   }
 
   componentDidMount() {
+    const tables = this.getTables();
     const notes = this.state.notes.sort((firstNote, secondNote) => {
       return firstNote.noteIndex - secondNote.noteIndex;
     });
-    this.setState({ notes });
+    this.setState({ notes, tables });
+  }
+
+  getTables() {
+    const tables = [];
+    this.state.notes.forEach(note => {
+      if (!tables.includes(note.noteTable)) {
+        tables.push(note.noteTable);
+      }
+    });
+    return tables;
   }
 
   swapNotes = async (sourceIdx, destinationIdx) => {
@@ -65,14 +108,12 @@ class NotesList extends Component {
   };
 
   renderNote = (note, isDragging, draggableProps) => (
-    <NoteContainer
+    <NoteContainerLink
       to={`/notes/${note.noteId}`}
       style={getItemStyle(isDragging, draggableProps)}
     >
-      <ListGroupItem style={{ backgroundColor: "#EFEFE1" }}>
-        {note.content}
-      </ListGroupItem>
-    </NoteContainer>
+      <NoteCard>{note.content}</NoteCard>
+    </NoteContainerLink>
   );
 
   onDragEnd = async result => {
@@ -88,39 +129,66 @@ class NotesList extends Component {
     );
 
     this.setState({ notes });
-
-    await this.swapNotes(result.source.index, result.destination.index);
   };
 
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(provided, snapshot) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {this.state.notes.map((note, index) => (
-                <Draggable
-                  key={note.noteId}
-                  draggableId={note.noteId}
-                  index={index}
-                >
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <ListGroup>
+        <TablesContainer>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{ backgroundColor: "white" }}
+              >
+                {this.state.notes.map((note, index) => (
+                  <Draggable
+                    key={note.noteId}
+                    draggableId={note.noteId}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
                         {this.renderNote(note, snapshot.isDragging)}
-                      </ListGroup>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+
+          <Droppable droppableId="droppable2">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{ backgroundColor: "white" }}
+              >
+                {this.state.test.map((e, index) => (
+                  <Draggable key={e} draggableId={e} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {this.renderNote(e, snapshot.isDragging)}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </TablesContainer>
       </DragDropContext>
     );
   }
