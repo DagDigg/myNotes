4bb04f6d1a0fe914157b100d4a2520e3cf0337e7
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-16 16:20:46 +0200

    :sparkles: Added createTable() and documented

--- src/API/tablesAPI.js
+++ src/API/tablesAPI.js
@@ -1,6 +1,11 @@
 import { API } from "aws-amplify";
 
 //POSTs & UPDATEs
+
+/**
+ * Creates the initial table on SignUp
+ * @return {Promise} Promise response
+ */
 export const postInitialTable = async () => {
   let result;
   try {
@@ -15,6 +20,32 @@ export const postInitialTable = async () => {
   return await result;
 };
 
+/**
+ * POST a new Table
+ * @param {string} tableName The name of the table
+ * @return {Promise} Promise response
+ */
+export const createTable = async tableName => {
+  let result;
+  try {
+    result = API.post("notes", "/tables", {
+      body: {
+        tableName: tableName
+      }
+    });
+  } catch (e) {
+    alert(e);
+  }
+  return await result;
+};
+
+/**
+ * Updates the desired table
+ * @param {string} tableId ID of the table
+ * @param {string} tableName Name of the table
+ * @param {Array} notes Notes Array to be updated
+ * @return {Promise} Promise response
+ */
 export const updateTable = async (tableId, tableName, notes) => {
   let result;
   try {
@@ -31,6 +62,12 @@ export const updateTable = async (tableId, tableName, notes) => {
 };
 
 //GETs & LISTs
+
+/**
+ * Gets the Table Object based on the tableId
+ * @param {string} tableId ID of the table
+ * @return {Promise} Promise response
+ */
 export const getTable = async tableId => {
   let result;
   try {
