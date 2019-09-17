8ac9a98eeb2802f1448f86d0325c0b45701ab929
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-17 12:49:09 +0200

    :lipstick: Styled LoaderButton

--- src/components/LoaderButton.js
+++ src/components/LoaderButton.js
@@ -1,21 +1,50 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
 import "./LoaderButton.css";
+import styled from "styled-components";
+
 export default ({
   isLoading,
   text,
   loadingText,
   disabled = false,
   ...props
-}) => (
-  <Button variant="flat" disabled={disabled || isLoading} {...props}>
-    <div>
-      {isLoading && (
-        <FontAwesomeIcon icon={faSync} size="lg" className="loaderButton" />
-      )}
-      {isLoading ? loadingText : text}
-    </div>
-  </Button>
-);
+}) => {
+  const Button = styled.button`
+    width: 240px;
+    height: 50px;
+
+    justify-content: center;
+    margin: 50px auto;
+    border: 2px solid;
+    background-color: transparent;
+    border-radius: 10px;
+    transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out;
+
+    &:hover:enabled {
+      background-color: #ff9f1c;
+      color: white;
+    }
+    &:enabled {
+      border-color: #ff9f1c;
+      color: #ff9d00;
+    }
+    & div {
+      font-size: 18px;
+      font-weight: bold;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+    }
+  `;
+  return (
+    <Button disabled={disabled || isLoading} {...props}>
+      <div>
+        {isLoading && (
+          <FontAwesomeIcon icon={faSync} size="lg" className="loaderButton" />
+        )}
+        {isLoading ? loadingText : text}
+      </div>
+    </Button>
+  );
+};
