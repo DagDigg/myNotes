df0dddc557a1279babd6d6ce8408bcaefcd19a8e
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-17 14:15:18 +0200

    :sparkles: Added updateNoteTable() PUT to notesAPI

--- src/API/notesAPI.js
+++ src/API/notesAPI.js
@@ -3,3 +3,27 @@ import { API } from "aws-amplify";
 export const getNotes = async () => {
   return await API.get("notes", "/notes");
 };
+
+/**
+ * PUTs noteTable property to note
+ * @param {Array} note The correct note to be updated
+ *
+ * @return {Promise} Promise response
+ */
+export const updateNoteTable = async note => {
+  const noteId = note.noteId;
+  let response;
+  try {
+    response = API.put("notes", `/notes/${noteId}`, {
+      body: {
+        content: note.content,
+        attachment: note.attachment,
+        noteTable: note.noteTable
+      }
+    });
+  } catch (e) {
+    alert(e);
+  }
+
+  return await response;
+};
