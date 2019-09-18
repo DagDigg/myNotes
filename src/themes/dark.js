e7f556e988868c97c4de659f33f08a8e986e62c7
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-18 13:15:13 +0200

    :sparkles: Created dark theme colors

---
+++ src/themes/dark.js
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    background: "#101010",
+    appBackground: "#222831",
+    primaryText: "#fd7014",
+    secondaryText: "#fd7014",
+    shadowColor: "none",
+    cardBackground: "#393e46",
+    noteColor: "#eeeeee",
+    buttonColor: "#fd7014"
+  }
+};
