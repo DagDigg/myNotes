c707ec958052a303112f0ad56a7692a84923aae8
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-18 13:15:28 +0200

    :sparkles: Created light theme colors

---
+++ src/themes/light.js
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    background: "#887962",
+    appBackground: "#ffedc5",
+    primaryText: "#584c3a",
+    secondaryText: "#555555",
+    shadowColor: "0px 0px 5px -1px #3f3a12;",
+    cardBackground: "#fffaf1",
+    noteColor: "#e2c188",
+    buttonColor: "#887962"
+  }
+};
