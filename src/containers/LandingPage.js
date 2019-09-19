ef536828093c61a206e5e94ed48064ce04e86235
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-19 15:14:25 +0200

    :lipstick: Styled LandingPage

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,27 +1,43 @@
 import React from "react";
 import styled from "styled-components";
+import NotesIllustration from "../Icons/NotesIllustration";
 
 const LandingPage = () => {
   const Container = styled.div`
-    min-height: 1000px;
-    vertical-align: middle;
+    min-height: 100%;
+    position: relative;
+  `;
+
+  const Illustration = styled.h1`
+    font-size: 800px;
+    position: absolute;
+    @media screen and (max-width: 1310px) {
+      & {
+        font-size: 600px;
+      }
+    }
   `;
 
   const Title = styled.h1`
-    font-size: 126px;
+    margin-top: 50px;
+    font-size: 60px;
+    z-index: 9999;
     color: ${props => props.theme.colors.primaryText};
-    text-align: left;
-    vertical-align: middle;
+    position: absolute;
   `;
+
+  const getIllustrationStyle = () => {
+    return {
+      position: "absolute",
+      left: "165px"
+    };
+  };
   return (
     <Container>
-      <Title>
-        Notes.
-        <br />
-        Made.
-        <br />
-        Easy.
-      </Title>
+      <Title>Notes. Made. Easy.</Title>
+      <Illustration>
+        <NotesIllustration style={getIllustrationStyle()} />
+      </Illustration>
     </Container>
   );
 };
