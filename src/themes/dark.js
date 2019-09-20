8cfdfcab734cb3c1a6a86775505c49e0888522b9
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-20 14:31:22 +0200

    :lipstick: Styled themes

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -2,11 +2,12 @@ export default {
   colors: {
     background: "#101010",
     appBackground: "#222831",
-    primaryText: "#fd7014",
+    primaryText: "#ff5722",
     secondaryText: "#fd7014",
     shadowColor: "none",
     cardBackground: "#393e46",
     noteColor: "#eeeeee",
+    draggingColor: "#DBDBDB",
     buttonColor: "#fd7014",
     disabledColor: "#6d6d6d"
   }
