d37eba42a35769dd2cbf3722a81d9c4a7a44d5ad
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-09-21 14:21:37 +0200

    :fire: Removed css import from LoaderButton

--- src/components/LoaderButton.js
+++ src/components/LoaderButton.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
-import "./LoaderButton.css";
 import styled from "styled-components";
 
 export default ({
