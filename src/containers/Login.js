78ae2f57ad390305ecd24af36918834a9ff3d19b
Claudio D'Agostino <claudio.dagdigg@gmail.com>
2019-10-25 18:51:32 +0200

    :recycle: Refactored Login, now it's a functional component with hooks

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,66 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FormGroup, FormControl } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { Auth } from "aws-amplify";
 import { Redirect } from "react-router-dom";
 import { AuthContainer, Label } from "../components/StyledAuthentication";
 import * as COSTANTS from "../config";
+import useLoginForm from "../hooks/useLoginForm";
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoading: false,
-      email: "",
-      password: "",
-      errDescription: ""
-    };
-  }
+const Login = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errDescription, setErrDescription] = useState("");
+  const { inputs, handleSubmit, handleInputChange } = useLoginForm(logUser);
 
   /**
    * Form validator. Returns true if email is > 0 and password > 8
    *
    * @return {Boolean}
    */
-  validateForm = () => {
+  const validateForm = () => {
     return (
-      this.state.email.length > 0 &&
-      this.state.password.length >= COSTANTS.MIN_PASSWORD_LENGTH
+      inputs.email &&
+      inputs.email.length > 0 &&
+      inputs.password &&
+      inputs.password.length >= COSTANTS.MIN_PASSWORD_LENGTH
     );
-  };
-
-  /**
-   * Handler for inputs. Sets the corrisponding id state to value
-   * @param {Object} e Event Object
-   */
-  handleChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
   };
 
   /**
    * Submit handler. Signs in user with email and password
    * @param {Object} e Event Object
    */
-  handleSubmit = async e => {
-    e.preventDefault();
-
-    this.setState({ isLoading: true });
+  async function logUser() {
+    setIsLoading(true);
     try {
-      await Auth.signIn(this.state.email, this.state.password);
-      this.props.userHasAuthenticated(true);
-    } catch (e) {
-      alert(e);
-      this.setState({
-        isLoading: false,
-        errDescription: e.message
-      });
+      await Auth.signIn(inputs.email, inputs.password);
+      props.userHasAuthenticated(true);
+    } catch (err) {
+      alert(err);
+      setIsLoading(false);
+      setErrDescription(err.message);
     }
-  };
+  }
 
   // Form Render
-  renderForm = () => (
-    <form onSubmit={this.handleSubmit}>
+  const renderForm = () => (
+    <form onSubmit={handleSubmit}>
       <FormGroup controlId="email">
         <Label>Email</Label>
         <FormControl
@@ -68,8 +52,8 @@ export default class Login extends Component {
           key="email"
           autoComplete="off"
           placeholder="Enter email"
-          value={this.state.email}
-          onChange={this.handleChange}
+          value={inputs.email || ""}
+          onChange={handleInputChange}
         />
       </FormGroup>
       <FormGroup controlId="password">
@@ -79,37 +63,37 @@ export default class Login extends Component {
           key="password"
           autoComplete="off"
           placeholder="Enter password"
-          value={this.state.password}
-          onChange={this.handleChange}
+          value={inputs.password || ""}
+          onChange={handleInputChange}
         />
       </FormGroup>
       <LoaderButton
         block
-        disabled={!this.validateForm()}
+        disabled={!validateForm()}
         type="submit"
-        isLoading={this.state.isLoading}
+        isLoading={isLoading}
         text="Login"
         loadingText="Logging in..."
       />
     </form>
   );
 
-  render() {
-    return (
-      <AuthContainer>
-        {this.state.errDescription ? (
-          <Redirect
-            to={{
-              pathname: "/err",
-              state: {
-                errDescription: this.state.errDescription
-              }
-            }}
-          />
-        ) : (
-          this.renderForm()
-        )}
-      </AuthContainer>
-    );
-  }
-}
+  return (
+    <AuthContainer>
+      {errDescription ? (
+        <Redirect
+          to={{
+            pathname: "/err",
+            state: {
+              errDescription: errDescription
+            }
+          }}
+        />
+      ) : (
+        renderForm()
+      )}
+    </AuthContainer>
+  );
+};
+
+export default Login;
